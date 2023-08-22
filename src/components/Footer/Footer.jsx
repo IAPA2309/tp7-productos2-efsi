78dcfba2d07586c782eef9f72f868c8a6e7424af
IAPA2309 <iniaki.perezaviles@gmail.com>
2023-08-21 23:46:15 -0300

    footer mid

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,24 +6,58 @@ function Footer() {
   return (
     <>
       <footer className="footer">
-        <section className="footer-section">
-          <h2>Ayuda</h2>
-          <p>Link 1</p>
-          <p>Link 2</p>
-          <p>Link 3</p>
-        </section>
-        <section className="footer-section">
-          <h2>Contacto</h2>
-          <p>Link 1</p>
-          <p>Link 2</p>
-          <p>Link 3</p>
-        </section>
-        <section className="footer-section">
-          <h2>Productos</h2>
-          <p>Link 1</p>
-          <p>Link 2</p>
-          <p>Link 3</p>
-        </section>
+        <div className="footer-space">
+          <div className="footer-flex">
+            <section className="footer-section">
+              <h4 className="footer-section-title">Ayuda</h4>
+              <nav>
+                <ul>
+                  <li>Centro de ayuda</li>
+                  <li>Opciones de cancelacion</li>
+                  <li>Problemas de entrega</li>
+                </ul>
+              </nav>
+            </section>
+            <section className="footer-section">
+              <h4 className="footer-section-title">Acerca de</h4>
+              <nav>
+                <ul>
+                  <li>Nuestra tienda</li>
+                  <li>Investor relations</li>
+                  <li>Blog</li>
+                </ul>
+              </nav>
+            </section>
+            <section className="footer-section">
+              <h4 className="footer-section-title">Redes sociales</h4>
+              <nav>
+                <ul>
+                  <li>Twitter</li>
+                  <li>Youtube</li>
+                  <li>Instagram</li>
+                </ul>
+              </nav>
+            </section>
+            <section className="footer-section">
+              <h4 className="footer-section-title">Productos</h4>
+              <nav>
+                <ul>
+                  <li>Categorias</li>
+                  <li>Publicar tu producto</li>
+                  <li>Opcion generica</li>
+                </ul>
+              </nav>
+            </section>
+          </div>
+          <section className="footer-section-extra">
+            <div className="footer-section-extra-items">
+              <div>
+                <p>© Iñaki P, Bautista S.</p>
+              </div>
+              <div>$ USD</div>
+            </div>
+          </section>
+        </div>
       </footer>
       <Outlet />
     </>
