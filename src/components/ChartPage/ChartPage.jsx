e48c232d9c07894854c83d98c87d9b2f8a3d92e1
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-15 12:08:45 -0300

    css no cart

--- src/components/ChartPage/ChartPage.jsx
+++ src/components/ChartPage/ChartPage.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { useCart } from '../../Context/ChartContext';
+import { Link } from "react-router-dom";
+import './ChartPage.css'
+import ShoppingCartTwoToneIcon from '@mui/icons-material/ShoppingCartTwoTone';
 
 function ChartPage() {
     const { cart, dispatch } = useCart();
@@ -14,23 +17,46 @@ function ChartPage() {
     };
   
     return (
-      <div className="cart">
-        <h2>Carrito de compras</h2>
-        {cart.length === 0 ? (
-          <p>Tu carrito está vacío</p>
-        ) : (
-          <ul>
-            {cart.map((product) => (
-              <li key={product.id}>
-                <span>{product.title}</span>
-                <span>${product.price}</span>
-                <button onClick={() => removeFromCart(product)}>Eliminar</button>
-              </li>
-            ))}
-          </ul>
-        )}
+      <div className='cart-container'>
+          <h2 className='cart-title'>Carrito de compras ({cart.length})</h2>
+        <div className="cart">
+          {cart.length === 0 ? (
+            <div className="flex flex-center">
+              <div>
+                <Link to="/products" className="seeProducts-btn">
+                  Ver productos
+                </Link>
+              </div>
+              <div className="chart-help">
+                <ShoppingCartTwoToneIcon
+                  sx={{ color: "#e82153", fontSize: 40 }}
+                />
+                <p>
+                  <span style={{ fontWeight: 700 }}>
+                    Tu carrito se encuentra vacío.{" "}
+                  </span>{" "}
+                  Una vez que agregues productos al carrito, se veran reflejados
+                  aqui.
+                </p>
+              </div>
+            </div>
+          ) : (
+            <ul className="flex">
+              {cart.map((product) => (
+                <li key={product.id}>
+                  <span>{product.title}</span>
+                  <span>{product.brand}</span>
+                  <span>${product.price}</span>
+                  <button onClick={() => removeFromCart(product)}>
+                    Eliminar
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
       </div>
-  )
+    );
 }
 
 export default ChartPage
